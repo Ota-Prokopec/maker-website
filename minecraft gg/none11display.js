ac3349d929d8f78da525dcdfbd621c4d44c43365
Ota-Prokopec <70975493+Ota-Prokopec@users.noreply.github.com>
2021-02-01 15:02:50 +0100

    Delete none11display.js

--- minecraft gg/none11display.js
+++
@@ -1,7 +0,0 @@
-function noneeverygg() {
-    document.getElementById("edit").style.display="none";
-    window.addEventListener("mousewheel", function () {
-      document.getElementById("edit").style.display="none";
-
-    })
-}
